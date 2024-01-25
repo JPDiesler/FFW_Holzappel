41e58d8064197b5002899818694b1af04cba021a
azubi-it@albertweil.de <azubi-it@albertweil.de>
2024-01-25 19:13:30 +0100

    Debugged slide transitions

--- src/components/modules/vehicles/Vehicles.jsx
+++ src/components/modules/vehicles/Vehicles.jsx
@@ -1,34 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Vehicles.scss";
 import { tlfw, dlk, hlf, mtf } from "./VehicleFrames";
 
 const Vehicles = () => {
   const vehicles = [tlfw, dlk, hlf, mtf];
   const [activeIndex, setActiveIndex] = useState(0);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [offset, setOffset] = useState(0);
+
+  console.log("T: " + windowWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+      console.log("w: " + window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const nextVehicle = () => {
-    setActiveIndex((activeIndex + 1) % vehicles.length);
+    const newIndex = (activeIndex + 1) % vehicles.length;
+    setActiveIndex(newIndex);
+    if (newIndex != 0) {
+      const offset_1 = windowWidth + 1035;
+      setOffset(offset - offset_1);
+    } else {
+      setOffset(0);
+    }
   };
 
   const prevVehicle = () => {
-    setActiveIndex((activeIndex - 1 + vehicles.length) % vehicles.length);
+    const offset_1 = windowWidth + 1035;
+    let newIndex = activeIndex - 1;
+    if (newIndex < 0) {
+      newIndex = vehicles.length - 1;
+      setOffset(-newIndex * offset_1);
+    } else {
+      setOffset(offset - offset_1);
+    }
+    setActiveIndex(newIndex);
   };
 
   return (
-    <div className="flex-fill d-flex flex-column justify-content-center align-items-center">
+    <div className="flex-fill d-flex flex-column justify-content-center align-items-center overflow-hidden">
       <h1 className="title">Unsere Einsatzfahrzeuge</h1>
       <div>
-        <div className="d-flex align-items-center position-relative border">
+        <div className="d-flex align-items-center position-relative">
           <button onClick={prevVehicle} className="btn p-0 pt-5 pb-5 me-3 z-1">
             <i className="bi bi-chevron-compact-left fs-1"></i>
           </button>
           <div className="placeholder z-1">{vehicles[0]}</div>
           <div
             id="carousel-container"
-            className="carousel-container border border-primary"
+            className="carousel-container"
             style={{
-              transform: `translateX(-${activeIndex * 100}vw)`,
-              transition: "transform 0.5s ease-in-out",
+              transform: `translateX(${offset}px)`,
+              transition: "transform 0.75s ease-in-out",
             }}
           >
             <button
