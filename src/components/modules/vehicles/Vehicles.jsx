8e37c333292d2d335b2606b96dd36e410c8bd5cf
azubi-it@albertweil.de <azubi-it@albertweil.de>
2024-01-25 01:22:02 +0100

    Getting closer

--- src/components/modules/vehicles/Vehicles.jsx
+++ src/components/modules/vehicles/Vehicles.jsx
@@ -15,23 +15,49 @@ const Vehicles = () => {
   };
 
   return (
-    <div className="flex-fill d-flex flex-column justify-content-center align-items-center border border-3 border-danger gap-3">
+    <div className="flex-fill d-flex flex-column justify-content-center align-items-center">
       <h1 className="title">Unsere Einsatzfahrzeuge</h1>
-      <div className="d-flex align-items-center">
-        <button onClick={prevVehicle}>Prev</button>
-        {vehicles[activeIndex]}
-        <button onClick={nextVehicle}>Next</button>
-      </div>
       <div>
-        {vehicles.map((vehicle, index) => (
-          <button
-            key={index}
-            style={{ backgroundColor: index === activeIndex ? "blue" : "gray" }}
-            onClick={() => setActiveIndex(index)}
-          >
-            {index + 1}
+        <div className="d-flex align-items-center position-relative border">
+          <button onClick={prevVehicle} className="btn p-0 pt-5 pb-5 me-3 z-1">
+            <i className="bi bi-chevron-compact-left fs-1"></i>
           </button>
-        ))}
+          <div className="placeholder z-1">{vehicles[0]}</div>
+          <div
+            id="carousel-container"
+            className="carousel-container border border-primary"
+            style={{
+              transform: `translateX(-${activeIndex * 100}vw)`,
+              transition: "transform 0.5s ease-in-out",
+            }}
+          >
+            <button
+              onClick={prevVehicle}
+              className="btn p-0 pt-5 pb-5 me-3 placeholder"
+              disabled={true}
+            >
+              <i className="bi bi-chevron-compact-left fs-1"></i>
+            </button>
+            {vehicles.map((vehicle) => {
+              return vehicle;
+            })}
+          </div>
+          {/* {vehicles[activeIndex]} */}
+          <button onClick={nextVehicle} className="btn p-0 pt-5 pb-5 ms-3 z-1">
+            <i className="bi bi-chevron-compact-right fs-1"></i>
+          </button>
+        </div>
+        <div className="flex-fill d-flex justify-content-center align-items-centerp-2 pt-3 pb-4">
+          {vehicles.map((vehicle, index) => (
+            <button
+              key={index}
+              className={`indicator ${
+                index === activeIndex ? "indicator_active " : ""
+              }`}
+              onClick={() => setActiveIndex(index)}
+            ></button>
+          ))}
+        </div>
       </div>
     </div>
   );
