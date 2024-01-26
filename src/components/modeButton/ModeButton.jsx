4590ea70052cca1c92067e76b15b9ae702265db8
azubi-it@albertweil.de <azubi-it@albertweil.de>
2024-01-26 23:06:09 +0100

    Refactor Modebutton

--- src/components/modeButton/ModeButton.jsx
+++ src/components/modeButton/ModeButton.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import "./ModeButton.scss";
 import bootstrap from "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { useState } from "react";
 
+const icons = {
+  ligth: <i className="bi bi-brightness-high-fill pe-1"></i>,
+  dark: <i className="bi bi-moon-stars-fill pe-1"></i>,
+  auto: <i className="bi bi-circle-half pe-1"></i>,
+};
+
 const ModeButton = () => {
+  const ulRef = useRef(null);
+  const buttonRef = useRef(null);
+  const html = document.getElementsByTagName("html")[0];
+  const [icon, setIcon] = useState(icons[html.getAttribute("data-bs-theme")]);
+  const [menu, setMenu] = useState(false);
+  const [mode, setMode] = useState(icons[html.getAttribute("data-bs-theme")]);
+
   useEffect(() => {
     var tooltipTriggerList = [].slice.call(
       document.querySelectorAll('[data-bs-toggle="tooltip"]')
@@ -14,15 +27,20 @@ const ModeButton = () => {
     });
   }, []);
 
-  const icons = {
-    ligth: <i className="bi bi-brightness-high-fill pe-1"></i>,
-    dark: <i className="bi bi-moon-stars-fill pe-1"></i>,
-    auto: <i className="bi bi-circle-half pe-1"></i>,
-  };
-  const html = document.getElementsByTagName("html")[0];
-  const [icon, setIcon] = useState(icons[html.getAttribute("data-bs-theme")]);
+  useEffect(() => {
+    function handleClickOutside(event) {
+      setMenu(false);
+    }
 
-  function onClick(mode) {
+    // Bind the event listener
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  function setColorMode(mode) {
     setIcon(icons[mode]);
     if (mode == "auto") {
       const prefersDarkScheme = window.matchMedia(
@@ -30,56 +48,86 @@ const ModeButton = () => {
       ).matches;
       if (prefersDarkScheme) {
         html.setAttribute("data-bs-theme", "dark");
+        setMode("dark");
       } else {
         html.setAttribute("data-bs-theme", "light");
+        setMode("light");
       }
     } else {
       html.setAttribute("data-bs-theme", mode);
+      setMode(mode);
     }
   }
 
   return (
-    <div className="dropdown-center">
+    <div className="position-relative">
       <button
-        className="btn text-light d-flex align-items-center"
+        ref={buttonRef}
+        className="btn text-light d-flex align-items-center justify-content-center"
         type="button"
-        data-bs-toggle="dropdown"
-        data-bs-auto-close="true"
+        onClick={(e) => {
+          e.stopPropagation();
+          setMenu(!menu);
+        }}
       >
-        <span className="border-end border-light pe-1">{icon}</span>
+        <span className="border-end border-light p1-1">{icon}</span>
         <span className="ps-1">
           <i className="bi bi-caret-down-fill"></i>
         </span>
       </button>
-      <ul className="dropdown-cs">
-        <li
-          className="d-flex align-items-center p-1"
-          onClick={() => onClick("ligth")}
-          data-bs-toggle="tooltip"
-          data-bs-placement="right"
-          title="Light Mode"
+      {menu ? (
+        <ul
+          ref={ulRef}
+          className={`modeMenu border rounded p-2 ${
+            mode == "light" ? "bg-body" : "bg-secondary-subtle"
+          }`}
         >
-          <i className="bi bi-brightness-high-fill pe-1"></i>
-        </li>
-        <li
-          className="d-flex align-items-center p-1"
-          onClick={() => onClick("dark")}
-          data-bs-toggle="tooltip"
-          data-bs-placement="right"
-          title="Dark Mode"
-        >
-          <i className="bi bi-moon-stars-fill pe-1"></i>
-        </li>
-        <li
-          className="d-flex align-items-center p-1"
-          onClick={() => onClick("auto")}
-          data-bs-toggle="tooltip"
-          data-bs-placement="right"
-          title="Automatisch"
-        >
-          <i className="bi bi-circle-half pe-1"></i>
-        </li>
-      </ul>
+          <li>
+            <button
+              className="btn d-flex align-items-center justify-content-center p-1"
+              onClick={() => {
+                setMenu(false);
+                setColorMode("ligth");
+              }}
+              data-bs-toggle="tooltip"
+              data-bs-placement="right"
+              title="Light Mode"
+            >
+              <i className="bi bi-brightness-high-fill p-1"></i>
+            </button>
+          </li>
+          <li>
+            <button
+              className="btn d-flex align-items-center justify-content-center p-1"
+              onClick={() => {
+                setMenu(false);
+                setColorMode("dark");
+              }}
+              data-bs-toggle="tooltip"
+              data-bs-placement="right"
+              title="Dark Mode"
+            >
+              <i className="bi bi-moon-stars-fill p-1"></i>
+            </button>
+          </li>
+          <li>
+            <button
+              className="btn d-flex align-items-center justify-content-center p-1"
+              onClick={() => {
+                setMenu(false);
+                setColorMode("auto");
+              }}
+              data-bs-toggle="tooltip"
+              data-bs-placement="right"
+              title="Automatisch"
+            >
+              <i className="bi bi-circle-half p-1"></i>
+            </button>
+          </li>
+        </ul>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
